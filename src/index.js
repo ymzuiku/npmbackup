6029ad98a27fd2afc8a5c37bd5b80303b21ba484
pillar <pillar.liang@bindo.com>
2018-07-26 10:45:53 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,57 +1,28 @@
-#!/usr/bin/env node
-/*
-* @Author: ym 
-* @Date: 2018-02-15 15:26:28 
- * @Last Modified by: ym
- * @Last Modified time: 2018-02-16 13:42:50
- */
-
-const package = require('../package.json');
-const options = require('./options');
 const { realFunc, doInit, doLoad, doSave } = require('./io');
-const argv = process.argv.splice(2);
 
-for (let i = 0, l = argv.length; i < l; i++) {
-  const ele = argv[i];
-  // config
-  if (ele === '-d' || ele === '--develop') {
-    options.isUseDevelop = true;
-  }
-  if (ele === '-v' || ele === '--version') {
-    console.log(package.version);
-  }
-  if (ele === '-h' || ele === '--hidden-log') {
-    options.isLogCopyFile = false;
-  }
-  if (ele === '-t' || ele === '--tag') {
-    if (argv.length >= i + 1) {
-      options.tag = argv[i + 1] || 'default';
-    } else {
-      console.log('no value of --tag');
-    }
-  }
-  if (ele === 'init') {
-    let param;
-    if (argv.length >= i + 1) {
-      param = argv[i + 1];
-    }
-    doInit(param);
-  } else if (ele === 'save') {
-    doSave();
-  } else if (ele === 'load') {
-    doLoad();
+function run() {
+  if (options.error) {
+    console.warn(options.error);
   } else {
-    // options.error = 'No input params';
+    console.time('Done');
+    console.log('Sync files...');
+    realFunc.doLoad();
+    realFunc.doSave();
+    console.log(`merged ${options.mergeFilesCount} files`);
+    console.timeEnd(`Done`);
   }
 }
 
-if (options.error) {
-  console.warn(options.error);
-} else {
-  console.time('Done')
-  console.log('Sync files...')
-  realFunc.doLoad();
-  realFunc.doSave();
-  console.log(`merged ${options.mergeFilesCount} files`);
-  console.timeEnd(`Done`);
+function api({ type, isDevelop, option }) {
+  options = { ...options, option };
+  if (type === 'init') {
+    doInit();
+  } else if (type === 'save') {
+    doSave();
+  } else if (type === 'load') {
+    doLoad();
+  }
+  run();
 }
+
+module.exports = api;
